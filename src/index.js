cbe2672392f45aadb7acd1e984a1f242f05711af
Antonio <antonio.c.m.eu@gmail.com>
2024-06-02 14:22:08 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const connectDB = require("./api/config/db");
 const routes = require("./api/routes");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 connectDB();
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use(cors());
 
 app.use(express.json());
 
